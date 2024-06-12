cf64d8357d0b83f936d827189cc05620a2470780
vanilla-dd <freeidlll6@gmail.com>
2024-06-12 23:19:06 +0530

    feat: navbar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import Navbar from "@/components/Navbar";
 
@@ -9,6 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Sprout",
   description: "Modern E-commerce Web app",
+  icons: "/logo.svg",
 };
 
 export default function RootLayout({
